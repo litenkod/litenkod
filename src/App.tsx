a49db77eb4798bd99cbbf5c87da2c7a05ae8cb15
Magnus Lindeberg <magnus.lindeberg@consid.se>
2025-10-02 12:21:33 +0200

    refactor: clean up class names in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="flex h-screen w-screen bg-teal-100">
-      <div className="w-full flex items-center justify-center flex-col  -mt-10">
+      <div className="w-full flex items-center justify-center flex-col -mt-10">
         <h1 className="font-display text-6xl">litenkod.se</h1>
         <p className="font-sans text-[0.8rem]">
           liten kod kan göra stora underverk
